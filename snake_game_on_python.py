4c286306db9bd73b8dbd4564351a1b013ce7fc6d
Samuel <samueldavetoledo678@gmail.com>
2026-03-02 15:38:52 +0800

    Importing of tools and variables for visuals

---
+++ snake_game_on_python.py
@@ -0,0 +1,18 @@
+import pygame 
+import sys
+import random
+import json
+import os
+
+pygame.init()
+
+window_width = 640
+header_height = 60
+
+color_background = (20, 20, 20)
+color_header_background = (10, 10, 10)
+color_normal_text = (255,255, 255)
+color_selected_text = (255,215,0)
+color_snake_body = (50, 205, 50)
+color_snake_head = (0, 155, 0)
+
